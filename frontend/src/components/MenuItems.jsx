94e4e5250a1305d90644aa22ceb081a2cea7b07e
satya-codes <63343224+satya-codes@users.noreply.github.com>
2021-06-21 17:58:04 +0530

    logo changed

--- frontend/src/components/MenuItems.jsx
+++ frontend/src/components/MenuItems.jsx
@@ -11,8 +11,8 @@ const MenuItems = ({onLogin}) => {
             <div className='menuItemLogo'><Link to='/'><img className="rotate" src="ethicon.svg" alt='ethicon'/></Link></div>
             <ul className='menuItems'>
                 <li className='menuItem'><Link to='/'>Home</Link></li>
-                <li className='menuItem'><Link to='/SimpleStorageDapp'>Data Storage</Link></li>
-                <li className='menuItem'><Link to='/BankDapp'>SAI Bank</Link></li>
+                <li className='menuItem'><Link to='/SimpleStorageDapp'>Data Storage Dapp</Link></li>
+                <li className='menuItem'><Link to='/BankDapp'>SAI Bank Dapp</Link></li>
                 
                 {/* <li className='menuItem'>Currency Exchange</li>
                 <li className='menuItem'><Link to='/Test'>Test</Link></li> */}
