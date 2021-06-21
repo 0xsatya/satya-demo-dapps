2fcc9c8178115dd32c99f9c36968742954baa7be
satya-codes <63343224+satya-codes@users.noreply.github.com>
2021-06-21 17:57:10 +0530

    logo changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer">
-            <span> 	&#169; Copyright 2021 (CodeBlocks) - to contact developer, please email at satyas2099@gmail.com </span>
+            <span> 	&#169; Copyright 2021 (DecodeBlocks) - to contact developer, please email at satyas2099@gmail.com </span>
         </div>
     )
 }
--- frontend/src/components/MenuItems.jsx
+++ frontend/src/components/MenuItems.jsx
@@ -7,11 +7,12 @@ import {
 const MenuItems = ({onLogin}) => {
     return (
         <div  className='menuItemsContainer'>
+            {/* <div className='menuItemLogo1'><Link to='/'><img className="decodeblockslogo" src="DecodeBlocks-bgBlack.png" alt='decodeblockslogo'/></Link></div> */}
             <div className='menuItemLogo'><Link to='/'><img className="rotate" src="ethicon.svg" alt='ethicon'/></Link></div>
             <ul className='menuItems'>
                 <li className='menuItem'><Link to='/'>Home</Link></li>
-                <li className='menuItem'><Link to='/SimpleStorageDapp'>SimpleStorage Contract</Link></li>
-                <li className='menuItem'><Link to='/BankDapp'>Bank Dapp</Link></li>
+                <li className='menuItem'><Link to='/SimpleStorageDapp'>Data Storage</Link></li>
+                <li className='menuItem'><Link to='/BankDapp'>SAI Bank</Link></li>
                 
                 {/* <li className='menuItem'>Currency Exchange</li>
                 <li className='menuItem'><Link to='/Test'>Test</Link></li> */}
